b40ec376ae853f6d7f9e0308c7bd8a60fdf47fb9
Ander <volintine_21001524@utp.edu.my>
2025-06-13 17:21:45 +0800

    minimal fetch example

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'screens/dashboard.dart';
 
-void main() {
-  runApp(const MainApp());
+Future main() async {
+    await dotenv.load(fileName: ".env");
+    final String url = dotenv.env['SUPABASE_URL']!;
+    final String key = dotenv.env['SUPABASE_KEY']!;
+    await Supabase.initialize(url: url, anonKey: key, debug: true);
+
+    runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+    const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
+    @override
+    Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+        title: 'Mantadsodu',
+        home: Dashboard(),
     );
-  }
+    }
 }
